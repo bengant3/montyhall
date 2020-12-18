8dd389a97b75a88021a7a045138c30eda0c037b9
bengant3 <bengant3@gmail.com>
2020-12-18 17:21:44 -0500

    monty hall problem simulator

---
+++ cmake-build-debug/CMakeFiles/montyhall.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/montyhall.dir/main.cpp.obj"
+  "libmontyhall.dll.a"
+  "montyhall.exe"
+  "montyhall.exe.manifest"
+  "montyhall.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/montyhall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,71 @@
+//Game show paradox simulator
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string again;
+    do {
+        uint32_t iter;
+        cout << "How many iterations?";
+        cin >> iter;
+
+        bool switchDoors = false;
+        string response;
+        cout << "Switch doors if given chance? (y/n)";
+        cin >> response;
+        if(response == "y" || response == "yes")
+            switchDoors = true;
+
+        bool watch = false;
+        cout << "watch simulation? (y/n)";
+        cin >> response;
+        if(response == "y" || response == "yes")
+            watch = true;
+
+        int results[iter];
+        int totalWins = 0;
+        for(int game : results) {
+            int prize = rand() % 3 + 1;
+            int choice = rand() % 3 + 1;
+            int shown;
+            bool win;
+
+            if(watch)
+                cout << endl << "Door " << choice << " was chosen." << endl;
+
+            if(prize == choice) {
+                //if the prize door was chosen, then 1 of the other 2 doors is randomly selected
+                shown = (prize + rand() % 2) % 3 + 1;
+                win = !switchDoors;
+            } else {
+                //if the prize door wasn't chosen, then the remaining incorrect door is chosen.
+                shown = 3 - (prize+choice)%3;
+                win = switchDoors;
+            }
+
+            if(watch)
+            {
+                cout << "Door " << shown << " was shown to not have the prize." << endl;
+
+                if(switchDoors && watch)
+                    cout  << "You switched to Door " << 3 - (shown+choice)%3 << endl;
+
+                cout << "Door " << prize << " had the prize, so you ";
+                if(win)
+                    cout << "won!" << endl;
+                else
+                    cout << "lost." << endl;
+            }
+            game = win;
+            totalWins += win;
+        }
+        cout << endl << "You won " << totalWins << " games out of " << iter << endl;
+
+        cout << endl << "Simulate again? (y/n) ";
+        cin >> again;
+
+    } while (again == "y" || again == "yes");
+}
